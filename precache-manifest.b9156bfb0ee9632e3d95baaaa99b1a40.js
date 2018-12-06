a8f2ab04e3fdc14259af2b2a2aa1aa948e654d47
george <ccchang0661@gmail.com>
2018-12-06 09:27:12 +0800

    Updates

--- precache-manifest.b9156bfb0ee9632e3d95baaaa99b1a40.js
+++ precache-manifest.b9156bfb0ee9632e3d95baaaa99b1a40.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/rent-house/static/media/daniil-silantev-574966-unsplash.22488c24.jpg"
   },
   {
-    "revision": "0f331d0e1f4039c533bf",
+    "revision": "7e24031c231a8380d921",
     "url": "/rent-house/static/css/main.400fea73.chunk.css"
   },
   {
@@ -16,24 +16,24 @@ self.__precacheManifest = [
     "url": "/rent-house/static/js/runtime~main.6e49e162.js"
   },
   {
+    "revision": "2ebb3a7c51dc74d4cf5492843f162b5a",
+    "url": "/rent-house/static/media/douglas-sheppard-522206-unsplash.2ebb3a7c.jpg"
+  },
+  {
     "revision": "1b70a2777c7f9ba5de3961fc78f78ef1",
     "url": "/rent-house/static/media/henry-co-1047241-unsplash.1b70a277.jpg"
+  },
+  {
+    "revision": "7e24031c231a8380d921",
+    "url": "/rent-house/static/js/main.7e24031c.chunk.js"
   },
   {
     "revision": "6ecfe967bbffc8c269d8903fca790f7c",
     "url": "/rent-house/static/media/hamed-alishahi-1108445-unsplash.6ecfe967.jpg"
   },
   {
-    "revision": "0f331d0e1f4039c533bf",
-    "url": "/rent-house/static/js/main.0f331d0e.chunk.js"
-  },
-  {
     "revision": "bb48ec2d1ddb9265f7439cd8ae501cd6",
     "url": "/rent-house/static/media/dan-gold-220226-unsplash.bb48ec2d.jpg"
-  },
-  {
-    "revision": "2ebb3a7c51dc74d4cf5492843f162b5a",
-    "url": "/rent-house/static/media/douglas-sheppard-522206-unsplash.2ebb3a7c.jpg"
   },
   {
     "revision": "af9706096804a0b697b6e4f02d7136de",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "/rent-house/static/media/remi-yuan-569408-unsplash.b1c181cf.jpg"
   },
   {
-    "revision": "b12f804efed54606611a7b8d6f10248e",
+    "revision": "7d58fa5696616f6fccd433f0f9e94ec8",
     "url": "/rent-house/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rent-house/precache-manifest.37c44685c1c17934bd84bb6b56e43789.js"
+  "/rent-house/precache-manifest.b9156bfb0ee9632e3d95baaaa99b1a40.js"
 );
 
 workbox.clientsClaim();
