c91e80c2be3e6162e07c3386f5110ea74ef1a3c7
george <ccchang0661@gmail.com>
2018-12-06 08:59:09 +0800

    Updates

---
+++ precache-manifest.37c44685c1c17934bd84bb6b56e43789.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "22488c24eb6132e01a39b410884eeb0a",
+    "url": "/rent-house/static/media/daniil-silantev-574966-unsplash.22488c24.jpg"
+  },
+  {
+    "revision": "0f331d0e1f4039c533bf",
+    "url": "/rent-house/static/css/main.400fea73.chunk.css"
+  },
+  {
+    "revision": "eac83ab0c7acdd23a63b",
+    "url": "/rent-house/static/js/1.eac83ab0.chunk.js"
+  },
+  {
+    "revision": "6e49e162ee2723d1861a",
+    "url": "/rent-house/static/js/runtime~main.6e49e162.js"
+  },
+  {
+    "revision": "1b70a2777c7f9ba5de3961fc78f78ef1",
+    "url": "/rent-house/static/media/henry-co-1047241-unsplash.1b70a277.jpg"
+  },
+  {
+    "revision": "6ecfe967bbffc8c269d8903fca790f7c",
+    "url": "/rent-house/static/media/hamed-alishahi-1108445-unsplash.6ecfe967.jpg"
+  },
+  {
+    "revision": "0f331d0e1f4039c533bf",
+    "url": "/rent-house/static/js/main.0f331d0e.chunk.js"
+  },
+  {
+    "revision": "bb48ec2d1ddb9265f7439cd8ae501cd6",
+    "url": "/rent-house/static/media/dan-gold-220226-unsplash.bb48ec2d.jpg"
+  },
+  {
+    "revision": "2ebb3a7c51dc74d4cf5492843f162b5a",
+    "url": "/rent-house/static/media/douglas-sheppard-522206-unsplash.2ebb3a7c.jpg"
+  },
+  {
+    "revision": "af9706096804a0b697b6e4f02d7136de",
+    "url": "/rent-house/static/media/tuan-nguyen-1176823-unsplash.af970609.jpg"
+  },
+  {
+    "revision": "b374ccfe1c5049dfdea17d94015f059b",
+    "url": "/rent-house/static/media/luca-micheli-422053-unsplash.b374ccfe.jpg"
+  },
+  {
+    "revision": "b1c181cfc8966410dfa200b5a5a15686",
+    "url": "/rent-house/static/media/remi-yuan-569408-unsplash.b1c181cf.jpg"
+  },
+  {
+    "revision": "b12f804efed54606611a7b8d6f10248e",
+    "url": "/rent-house/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c3e20e5527f98e12547ad4494b4918a.js"
+  "/rent-house/precache-manifest.37c44685c1c17934bd84bb6b56e43789.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/rent-house/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
